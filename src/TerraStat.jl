b2f8e2fd194705292a4006172b916c999f565709
Matt Helm <mthelm85@gmail.com>
2024-08-21 11:30:08 -0700

    add buffer kward to qcew function

--- src/TerraStat.jl
+++ src/TerraStat.jl
@@ -87,11 +87,11 @@ function laus(shapefile_path::String, api_key::String; measure::Integer=3, pred:
     return leftjoin(counties, df; on=:GEOID)
 end
 
-function qcew(shapefile_path::String, api_key::String; data_type::Integer=1, size::Integer=0, ownership::Integer=5, industry::Integer=10, pred::Symbol=:intersects)
+function qcew(shapefile_path::String, api_key::String; data_type::Integer=1, size::Integer=0, ownership::Integer=5, industry::Integer=10, pred::Symbol=:intersects, buffer::Float64=0.09)
     if pred == :intersects
         counties = intersecting_counties(shapefile_path)
     elseif pred == :contains
-        counties = contained_counties(shapefile_path)
+        counties = contained_counties(shapefile_path, buffer)
     else
         error("Invalid predicate: $pred")
     end
