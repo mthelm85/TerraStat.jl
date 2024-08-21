8ee1d3ef577c406ba016c3b99c22b83817d633ab
Matt Helm <mthelm85@gmail.com>
2024-08-21 08:17:42 -0700

    use numeric codes for data options, add support for qcew

--- src/TerraStat.jl
+++ src/TerraStat.jl
@@ -21,7 +21,7 @@ function intersecting_counties(shapefile_path::String)
     return filter(row -> any([AG.intersects(user_shape.geometry[i], row.geometry) for i in 1:size(user_shape,1)]), counties)
 end
 
-function laus(shapefile_path::String, api_key::String; measure::Symbol=:unemployment_rate, pred::Symbol=:intersects)
+function laus(shapefile_path::String, api_key::String; measure::Integer=3, pred::Symbol=:intersects)
     if pred == :intersects
         counties = intersecting_counties(shapefile_path)
     elseif pred == :contains
@@ -30,18 +30,73 @@ function laus(shapefile_path::String, api_key::String; measure::Symbol=:unemploy
         error("Invalid predicate: $pred")
     end
 
-    if measure == :unemployment_rate
-        series_ids = ["LAUCN$(row.GEOID)0000000003" for row in eachrow(counties)]
-    elseif measure == :unemployment
-        series_ids = ["LAUCN$(row.GEOID)0000000004" for row in eachrow(counties)]
-    elseif measure == :employment
-        series_ids = ["LAUCN$(row.GEOID)0000000005" for row in eachrow(counties)]
-    elseif measure == :labor_force
-        series_ids = ["LAUCN$(row.GEOID)0000000006" for row in eachrow(counties)]
-    else
-        error("Invalid measure: $measure")
+    series_ids = ["LAUCN$(row.GEOID)000000000$(measure)" for row in eachrow(counties)]
+    url = "https://api.bls.gov/publicAPI/v2/timeseries/data"
+    headers = Dict("Content-Type" => "application/json")
+    all_rows = []
+
+    # Split series_ids into chunks of 50 since BLS API has a limit of 50 series per call
+    chunks = Iterators.partition(series_ids, 50)
+    total_calls = length(chunks)
+    current_call = 1
+
+    for chunk in chunks
+        @info "Performing call $current_call of $total_calls to the BLS API..."
+        payload = JSON.json(
+            Dict(
+                "seriesid" => chunk,
+                "registrationkey" => api_key,
+                "latest" => "true"
+            )
+        )
+
+        response = HTTP.post(url, headers=headers, body=payload)
+
+        if HTTP.status(response) == 200
+            try
+                data = JSON.parse(String(HTTP.body(response)))
+                series_data = data["Results"]["series"]
+
+                # Extract data points and add to all_rows
+                for series in series_data
+                    series_id = series["seriesID"]
+                    for data_point in series["data"]
+                        push!(all_rows, (
+                            seriesID = series_id,
+                            year = data_point["year"],
+                            period = data_point["period"],
+                            periodName = data_point["periodName"],
+                            latest = data_point["latest"],
+                            value = parse(Float64, data_point["value"]),
+                            footnotes = join([fn["text"] for fn in data_point["footnotes"]], ", ")
+                        ))
+                    end
+                end
+            catch e
+                error(e)
+            end
+        else
+            error("Error: ", HTTP.status(response), " - ", String(HTTP.body(response)))
+        end
+        current_call += 1
     end
 
+    df = DataFrame(all_rows)
+	df.GEOID = [row.seriesID[6:10] for row in eachrow(df)]
+
+    return leftjoin(counties, df; on=:GEOID)
+end
+
+function qcew(shapefile_path::String, api_key::String; data_type::Integer=1, size::Integer=0, ownership::Integer=5, industry::Integer, pred::Symbol=:intersects)
+    if pred == :intersects
+        counties = intersecting_counties(shapefile_path)
+    elseif pred == :contains
+        counties = contained_counties(shapefile_path)
+    else
+        error("Invalid predicate: $pred")
+    end
+
+    series_ids = ["ENU$(row.GEOID)$(data_type)$(size)$(ownership)$(industry)" for row in eachrow(counties)]
     url = "https://api.bls.gov/publicAPI/v2/timeseries/data"
     headers = Dict("Content-Type" => "application/json")
     all_rows = []
