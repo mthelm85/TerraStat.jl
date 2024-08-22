6f465dbcfaf6b3fec7929f2989a2be8c13d2937a
Matt Helm <mthelm85@gmail.com>
2024-08-22 12:07:07 -0700

    revert back to pure instersects check

--- src/TerraStat.jl
+++ src/TerraStat.jl
@@ -18,14 +18,8 @@ end
 function intersecting_geometries(user_shapefile_path::String, shapefile_path::String)
     geometries = GDF.read(project_path(shapefile_path))
     user_shape = GDF.read(user_shapefile_path)
-    return filter(row -> any([AG.intersects(user_shape.geometry[i], row.geometry) && AG.geomarea(AG.intersection(user_shape.geometry[i], row.geometry)) > 0.01 * AG.geomarea(row.geometry) for i in 1:size(user_shape, 1)]), geometries)
+    return filter(row -> any([AG.intersects(user_shape.geometry[i], row.geometry) for i in 1:size(user_shape,1)]), geometries)
 end
-
-# function intersecting_geometries(user_shapefile_path::String, shapefile_path::String)
-#     geometries = GDF.read(project_path(shapefile_path))
-#     user_shape = GDF.read(user_shapefile_path)
-#     return filter(row -> any([AG.intersects(user_shape.geometry[i], row.geometry) for i in 1:size(user_shape,1)]), geometries)
-# end
 
 function get_geometries(user_shapefile_path::String, pred::Symbol, buffer::Float64, shapefile_path::String)
     if pred == :intersects
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ test_geojson_path = TerraStat.project_path("data/research_triangle.geojson")
 
 @testset "intersecting_geometries function" begin
     result = TerraStat.intersecting_geometries(test_geojson_path, "data/cb_2018_us_county_5m.shp")
-    @test size(result, 1) == 3
+    @test size(result, 1) == 4
     @test size(result, 2) == 10
 end
 
@@ -18,7 +18,7 @@ end
 @testset "laus function" begin
     api_key = ENV["BLS_KEY"]
     result = TerraStat.laus(test_geojson_path, api_key)
-    @test size(result, 1) == 3
+    @test size(result, 1) == 4
     @test size(result, 2) == 17
     result_contains = TerraStat.laus(test_geojson_path, api_key, pred=:contains)
     @test size(result_contains, 1) == 0
@@ -28,7 +28,7 @@ end
 @testset "qcew function" begin
     api_key = ENV["BLS_KEY"]
     result = TerraStat.qcew(test_geojson_path, api_key)
-    @test size(result, 1) == 3
+    @test size(result, 1) == 4
     @test size(result, 2) == 17
     result_contains = TerraStat.laus(test_geojson_path, api_key, pred=:contains)
     @test size(result_contains, 1) == 0
