a85f2df14a71547ffa4ae89c1395cf688df6f030
Matt Helm <mthelm85@gmail.com>
2024-09-04 09:27:26 -0700

    remove latest=true option to get full time series

--- profile/contained_counties.jl
+++
@@ -1,5 +0,0 @@
-using TerraStat
-
-test_geojson_path = TerraStat.project_path("data/research_triangle.geojson")
-
-@profview TerraStat.contained_counties(test_geojson_path)
--- src/TerraStat.jl
+++ src/TerraStat.jl
@@ -47,7 +47,7 @@ function get_data(series_ids::Vector{String}, api_key::String, area_idx::UnitRan
             Dict(
                 "seriesid" => chunk,
                 "registrationkey" => api_key,
-                "latest" => "true"
+                # "latest" => "true"
             )
         )
 
@@ -67,7 +67,7 @@ function get_data(series_ids::Vector{String}, api_key::String, area_idx::UnitRan
                             year = data_point["year"],
                             period = data_point["period"],
                             periodName = data_point["periodName"],
-                            latest = data_point["latest"],
+                            # latest = data_point["latest"],
                             value = tryparse(Float64, data_point["value"]),
                             footnotes = join([fn["text"] for fn in data_point["footnotes"] if haskey(fn, "text")], ", ")
                         ))
